e216a7df6c9ea776adafe3010b03758d599c1761
maxi <maxi1985.798@gmail.com>
2019-11-17 16:19:14 -0300

    test 17 pass

--- benderBotLogic.py
+++ benderBotLogic.py
@@ -1,3 +1,6 @@
+import random
+import functools
+
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 ranks = ["narf", "Ace", "2", "3", "4", "5", "6", "7",
              "8", "9", "10", "Jack", "Queen", "King"]
@@ -7,11 +10,11 @@ class Card:
         self.rank = rank
 
 class Deck:
-    def __init__(self,aRandomGenerator):
-        self.cards = []
-        for suit in range(4):
-            for rank in range(1, 14):
-                self.cards.append(Card(suits[suit],ranks[rank]))
+    def __init__(self,aRandomGenerator,aSortedlistOfCards):
+        self.cards = aSortedlistOfCards
+        # for suit in range(4):
+        #     for rank in range(1, 14):
+        #         self.cards.append(Card(suits[suit],ranks[rank]))
         self.__randomGenerator = aRandomGenerator
     def print_deck(self):
         for card in self.cards:
@@ -91,66 +94,100 @@ class Dealer:
         return self.__deck.deal()
 
 class Player:
-    def __init__(self,name,playerHand,aDealer):
-        self.__playerName = name
+    def __init__(self,id,playerHand,aDealer):
+        self.__id = id
         self.__playerHand = playerHand
+        self.__dealer = aDealer
     def playerHand(self):
         return self.__playerHand
-    def name(self):
-        return self.__playerName
+    def id(self):
+        return self.__id
+    def takeHit(self):
+        card = self.__dealer.deal()
+        self.__playerHand.addCard(card)
+    def getCards(self):
+        return self.__playerHand.getCards()
+    def handValue(self):
+        return self.__playerHand.value()
 class BlackJackGame:
     def __init__(self,deck,dealer,players):
         self.__deck = deck
         self.__dealer = dealer
         self.__players = players
+        self.__finished = False
         # for p in players:
         #     self.__playersAndHands[p] = BlackJackHand(self.__deck.deal(),self.__deck.deal())
         self.__currentPlayer = players[0]
+    def currentPlayer(self):
+        return self.__currentPlayer
     def currentPlayerStand(self):
-        # self.__dealer.playerStand()
-        for i in range(len(self.__players)):
-            if i == (len(self.__players) -1):
-                self.__currentPlayer = self.__dealer
-                break
-            elif self.__players[i] == self.__currentPlayer:
-                self.__currentPlayer = self.__players[i+1]
-                break
+        if self.__currentPlayer == self.__dealer :
+            self.__finished = True
+        else:
+            for i in range(len(self.__players)):
+                if i == (len(self.__players) -1):
+                    self.__currentPlayer = self.__dealer
+                    break
+                elif self.__players[i] == self.__currentPlayer:
+                    self.__currentPlayer = self.__players[i+1]
+                    break
         
-    def isPlaying(self,aPlayer):
-        return self.__currentPlayer == aPlayer
-
-
-
-# class Chips:
+    def isCurrentPlayer(self,playerId):
+        return (self.__currentPlayer != self.__dealer) and (self.__currentPlayer.id() == playerId)
+    def isPlaying(self,playerId):
+        return any(p.id() == playerId for p in self.__players)
+    def currentPlayerTakeHit(self):
+        self.__currentPlayer.takeHit()
+        if self.__currentPlayer.handValue() > 21:
+            self.currentPlayerStand()
     
-#     def __init__(self,initialChips):
-#         self.total = initialChips  # This can be set to a default value or supplied by a user input
-#         self.bet = 0
-        
-#     def win_bet(self):
-#         self.total += self.bet
+    def cardsOf(self,playerId):
+        for p in self.__players:
+            if p.id() == playerId:
+                return p.getCards()
+
+    def isFinished(self):
+        return self.__finished
+    def currentPlayers(self):
+        return self.__players
+
+class Bender:
+
+    def __init__(self):
+        self.__games = []
+    def createGame(self,aDeck,playersId):
+        self.validateIfPlayersAreNotPlaying(playersId)
+        aDealerHand = BlackJackDealerHand(aDeck.deal())
+        aDealer = Dealer(aDeck,aDealerHand)
+        players = []
+        for pId in playersId:
+            aPlayerHand = BlackJackHand(aDeck.deal(),aDeck.deal())
+            players.append(Player(pId,aPlayerHand,aDealer))
+        aBlackJackGame = BlackJackGame(aDeck,aDealer,players)
+        self.__games.append(aBlackJackGame)
     
-#     def lose_bet(self):
-#         self.total -+ self.bet
+    @staticmethod
+    def playerCanNotBeInMultipleGamesErrorMessage():
+        return 'A player can not be in multiple games'
+    def validateIfPlayersAreNotPlaying(self,playersId):
+        if any(self.isPlayingInSomeGame(p,self.__games) for p in playersId):
+            raise PlayerCanNotBeInMultipleGamesException(Bender.playerCanNotBeInMultipleGamesErrorMessage())
+    def isPlayingInSomeGame(self,aPlayerId, games):
+        return any(g.isPlaying(aPlayerId) for g in games)
+    def getBlackjGames(self):
+        return self.__games
+    def takeHitFor(self,ThePlayerIdWhohWantToTakeHit):
+        gameOfThePlayer = list(filter(lambda g: g.isPlaying(ThePlayerIdWhohWantToTakeHit), self.__games))[0]
+        if gameOfThePlayer.isCurrentPlayer(ThePlayerIdWhohWantToTakeHit):
+            gameOfThePlayer.currentPlayerTakeHit()
+        else:
+            raise NotPlayerTurnsException('It is not player ' + ThePlayerIdWhohWantToTakeHit + ' turns')
+        return gameOfThePlayer
 
+def checkIfItIsPlaying():
+    return True
+class NotPlayerTurnsException(Exception):
+    pass
 
-
-
-class BenderBot:
-
-    def __init__(self,token):
-        self.token = token
-        self.hookers = ['https://i.imgur.com/VSP8EBp.jpg',
-            'https://i.imgur.com/Z5pYMOB.jpg', 
-            'https://i.imgur.com/BmAr75a.jpg',
-            'https://i.imgur.com/KqKqV0w.jpg',
-            'https://i.imgur.com/zg07iek.jpg',
-            'https://i.imgur.com/lR6MiJK.jpg',
-            'https://i.imgur.com/oulOY.jpg',
-            'https://i.imgur.com/bli8Q5c.jpg',
-            'https://tenor.com/view/pretty-woman-julia-roberts-richard-gere-gif-5736316'
-            ]
-        # self.deck = Deck()
-
-# d = Deck()
-# print(d)
+class PlayerCanNotBeInMultipleGamesException(Exception):
+    pass
--- bender_bot_test.py
+++ bender_bot_test.py
@@ -2,24 +2,38 @@ from benderBotLogic import *
 import unittest
 import random
 
+class FakeRandomGenerator:
+    def shuffle(self,aList):
+        pass
+
 class BenderBotTest(unittest.TestCase):
+
+    def getListOfSortedCards(self):
+        cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                cards.append(Card(suits[suit],ranks[rank]))
+        return cards
     
     def test_001_a_player_hand_has_only_2_cards_when_it_starts(self):
-        rng = random.Random()        # Create a random generator
-        aDeck = Deck(rng)
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
         playerHand = BlackJackHand(aDeck.deal(),aDeck.deal())
         self.assertEqual(2,len(playerHand.getCards()))
 
     def test_002_a_player_hand_has_only_3_cards(self):
-        rng = random.Random()        # Create a random generator
-        aDeck = Deck(rng)
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
         playerHand = BlackJackHand(aDeck.deal(),aDeck.deal())
         playerHand.addCard(aDeck.deal())
         self.assertEqual(3,len(playerHand.getCards()))
 
     def test_003_a_player_hand_has_only_4_cards(self):
-        rng = random.Random()        # Create a random generator
-        aDeck = Deck(rng)
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
         playerHand = BlackJackHand(aDeck.deal(),aDeck.deal())
         playerHand.addCard(aDeck.deal())
         playerHand.addCard(aDeck.deal())
@@ -50,73 +64,156 @@ class BenderBotTest(unittest.TestCase):
         self.assertEquals(len(dealerHand.getCards()),1)
     
     def test_008_dealer_must_add_a_card_when_he_takes_hit(self):
-        rng = random.Random()        # Create a random generator
-        aDeck = Deck(rng)
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
+        aDealerHand = BlackJackDealerHand(aDeck.deal())
+        aDealer = Dealer(aDeck,aDealerHand)
+        twoCard = Card("Clubs","2")
+        threeCard = Card("Clubs","3")
+        playerHand01 = BlackJackHand(twoCard,threeCard)
+        playerHand02 = BlackJackHand(aDeck.deal(),aDeck.deal())
+        playerHand03 = BlackJackHand(aDeck.deal(),aDeck.deal())
+        player01 = Player("player01id",playerHand01,aDealer)
+        player02 = Player("player02id",playerHand02,aDealer)
+        player03 = Player("player03id",playerHand03,aDealer)
+        players = [player01,player02,player03]
+        aBlackJackGame = BlackJackGame(aDeck,aDealer,players)
+        aBlackJackGame.currentPlayerTakeHit()
+        self.assertTrue(aBlackJackGame.isCurrentPlayer("player01id"))
+        self.assertEqual(3,len(aBlackJackGame.cardsOf("player01id")))
+
+    def test_009_when_a_player_has_more_than_21_dealer_must_pass(self):
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
         aDealerHand = BlackJackDealerHand(aDeck.deal())
         aDealer = Dealer(aDeck,aDealerHand)
         playerHand01 = BlackJackHand(aDeck.deal(),aDeck.deal())
         playerHand02 = BlackJackHand(aDeck.deal(),aDeck.deal())
         playerHand03 = BlackJackHand(aDeck.deal(),aDeck.deal())
-        player01 = Player("player01 name",playerHand01,aDealer)
-        player02 = Player("player02 name",playerHand02,aDealer)
-        player03 = Player("player03 name",playerHand03,aDealer)
+        player01 = Player("player01id",playerHand01,aDealer)
+        player02 = Player("player02id",playerHand02,aDealer)
+        player03 = Player("player03id",playerHand03,aDealer)
         players = [player01,player02,player03]
         aBlackJackGame = BlackJackGame(aDeck,aDealer,players)
-        self.assertTrue(aBlackJackGame.isPlaying(player01))
+        aBlackJackGame.currentPlayerTakeHit()
+        self.assertTrue(aBlackJackGame.isCurrentPlayer("player02id"))
+        self.assertEqual(3,len(aBlackJackGame.cardsOf("player01id")))
+        self.assertEqual(2,len(aBlackJackGame.cardsOf("player02id")))
+        self.assertTrue(player01.handValue()>21)
+
+    def test_010_game_is_finished_when_dealer_stand(self):
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
+        jackCard = Card("Clubs","Jack")
+        queenCard = Card("Clubs","Queen")
+        sevenCard = Card("Clubs","7")
+        playerHand01 = BlackJackHand(queenCard,jackCard)
+        aDealerHand = BlackJackDealerHand(sevenCard)
+        aDealer = Dealer(aDeck,aDealerHand)
+        player01 = Player("player01 name",playerHand01,aDealer)
+        players = [player01]
+        aBlackJackGame = BlackJackGame(aDeck,aDealer,players)
         aBlackJackGame.currentPlayerStand()
-        self.assertTrue(aBlackJackGame.isPlaying(player02))
-        # aDealer.deal()
-        # # self.assertEquals(aDealer.currentPlayer(),1)
-        # self.assertEquals(len(playerHand01.getCards()),3)
-        # self.assertEquals(len(playerHand02.getCards()),2)
-        # self.assertEquals(len(playerHand03.getCards()),2)
-        # self.assertEquals(len(aDealerHand.getCards()),1)
-        # aDealer.deal()
-        # # aDealer.playerStand()
-        # self.assertEquals(len(playerHand01.getCards()),3)
-        # self.assertEquals(len(playerHand02.getCards()),3)
-        # self.assertEquals(len(playerHand03.getCards()),2)
-        # self.assertEquals(len(aDealerHand.getCards()),1)
-        # aDealer.playerStand()
-        # self.assertEquals(len(playerHand01.getCards()),3)
-        # self.assertEquals(len(playerHand02.getCards()),3)
-        # self.assertEquals(len(playerHand03.getCards()),2)
-        # self.assertEquals(len(aDealerHand.getCards()),1)
-        # aDealer.deal()
-        # self.assertEquals(len(playerHand01.getCards()),3)
-        # self.assertEquals(len(playerHand02.getCards()),3)
-        # self.assertEquals(len(playerHand03.getCards()),2)
-        # self.assertEquals(len(aDealerHand.getCards()),2)
+        aBlackJackGame.currentPlayerStand()
+        self.assertTrue(aBlackJackGame.isFinished())
 
-    # def test_009_when_a_player_has_more_than_21_dealer_must_pass(self):
-    #     rng = random.Random()        # Create a random generator
-    #     aDeck = Deck(rng)
-        # aDealerHand = BlackJackDealerHand(aDeck.deal())
-        # playerHand01 = BlackJackHand(aDeck.deal(),aDeck.deal())
-        # playerHand02 = BlackJackHand(aDeck.deal(),aDeck.deal())
-        # playerHands = [playerHand01,playerHand02]
-        # aDealer = Dealer(aDeck,aDealerHand,playerHands)
-        # self.assertEquals(aDealer.currentPlayer(),0)
-        # while playerHand01.value() <= 21 :
-        #     aDealer.deal()
-        # self.assertEquals(aDealer.currentPlayer(),1)
-    # def test_010_when_a_player_has_more_than_dealer_player_wins(self):
-    #     rng = random.Random()        # Create a random generator
-        # aDeck = Deck(rng)
-        # jackCard = Card("Clubs","Jack")
-        # queenCard = Card("Clubs","Queen")
-        # sevenCard = Card("Clubs","7")
-        # playerHand01 = BlackJackHand(queenCard,jackCard)
-        # aDealerHand = BlackJackDealerHand(sevenCard)
-        # playerHands = [playerHand01]
-        # aDealer = Dealer(aDeck,aDealerHand)
-        # player01 = Player("player01 name",playerHand01,aDealer)
-        # players = [player01]
-        # aBlackJackGame = BlackJackGame(aDeck,aDealer,players)
-        # aBlackJackGame.currentPlayerStand()
-        # aBlackJackGame.currentPlayerStand()
-        # self.assertEquals(aBlackJackGame,1)
+    def test_011_game_is_not_finished_when_game_starts(self):
+        cards = self.getListOfSortedCards()
+        rng = FakeRandomGenerator()        # Create a random generator
+        aDeck = Deck(rng,cards)
+        jackCard = Card("Clubs","Jack")
+        queenCard = Card("Clubs","Queen")
+        sevenCard = Card("Clubs","7")
+        playerHand01 = BlackJackHand(queenCard,jackCard)
+        aDealerHand = BlackJackDealerHand(sevenCard)
+        aDealer = Dealer(aDeck,aDealerHand)
+        player01 = Player("player01 name",playerHand01,aDealer)
+        players = [player01]
+        aBlackJackGame = BlackJackGame(aDeck,aDealer,players)
+        self.assertFalse(aBlackJackGame.isFinished())
 
+    def test_012_bender_can_starts_a_BlackJackGame_with_one_player(self):
+        cards = self.getListOfSortedCards()
+        player01Id = "111111"
+        benderBot = Bender()
+        aDeck = Deck(FakeRandomGenerator(),cards)
+        benderBot.createGame(aDeck,[player01Id])
+        blackjGames = benderBot.getBlackjGames()
+        self.assertTrue(len(blackjGames) == 1)
+    
+    def test_013_bender_can_starts_a_BlackJackGame_with_two_player(self):
+        cards = self.getListOfSortedCards()
+        player01Id = "111111"
+        player02Id = "222222"
+        benderBot = Bender()
+        aDeck = Deck(FakeRandomGenerator(),cards)
+        benderBot.createGame(aDeck,[player01Id,player02Id])
+        blackjGames = benderBot.getBlackjGames()
+        self.assertTrue(len(blackjGames) == 1)
+        self.assertTrue(len(blackjGames[0].currentPlayers()) == 2)
+
+    def test_014_when_the_current_player_take_hit_should_have_3_cards(self):
+        cards = self.getListOfSortedCards()
+        player01Id = "111111"
+        player02Id = "222222"
+        benderBot = Bender()
+        aDeck = Deck(FakeRandomGenerator(),cards)
+        benderBot.createGame(aDeck,[player01Id,player02Id])
+        benderBot.takeHitFor(player01Id)
+        blackjGames = benderBot.getBlackjGames()
+        self.assertTrue(len(blackjGames[0].cardsOf(player01Id)) == 3)
+        self.assertTrue(len(blackjGames[0].cardsOf(player02Id)) == 2)
+        
+    def test_015_when_not_a_current_player_take_hit_should_inform_an_error(self):
+        cards = self.getListOfSortedCards()
+        player01Id = "111111"
+        player02Id = "222222"
+        benderBot = Bender()
+        aDeck = Deck(FakeRandomGenerator(),cards)
+        benderBot.createGame(aDeck,[player01Id,player02Id])
+        with self.assertRaises(NotPlayerTurnsException) as cm:
+            benderBot.takeHitFor(player02Id)
+        
+        self.assertEqual('It is not player '+player02Id+' turns',str(cm.exception))
+
+    def test_016_a_player_can_not_be_in_two_games(self):
+        cards = self.getListOfSortedCards()
+        player01Id = "111111"
+        player02Id = "222222"
+        player03Id = "222222"
+        benderBot = Bender()
+        aDeck = Deck(FakeRandomGenerator(),cards)
+        benderBot.createGame(aDeck,[player01Id,player02Id])
+        with self.assertRaises(PlayerCanNotBeInMultipleGamesException) as cm:
+            benderBot.createGame(aDeck,[player03Id,player02Id])
+        
+        self.assertEqual(Bender.playerCanNotBeInMultipleGamesErrorMessage(),str(cm.exception))
+    def test_017_different_player_can_play_in_different_games(self):
+        cards0 = self.getListOfSortedCards()
+        cards1 = self.getListOfSortedCards()
+        cards2 = self.getListOfSortedCards()
+        player01Id = "111111"
+        player02Id = "222222"
+        player03Id = "333333"
+        benderBot = Bender()
+        aDeck0 = Deck(FakeRandomGenerator(),cards0)
+        aDeck1 = Deck(FakeRandomGenerator(),cards1)
+        aDeck2 = Deck(FakeRandomGenerator(),cards2)
+        benderBot.createGame(aDeck0,[player01Id])
+        benderBot.createGame(aDeck1,[player02Id])
+        benderBot.createGame(aDeck2,[player03Id])
+        benderBot.takeHitFor(player01Id)
+        benderBot.takeHitFor(player02Id)
+        benderBot.takeHitFor(player03Id)
+        blackjGames = benderBot.getBlackjGames()
+        self.assertTrue(len(blackjGames) == 3)
+        self.assertTrue(len(blackjGames[0].currentPlayers()) == 1)
+        self.assertTrue(len(blackjGames[1].currentPlayers()) == 1)
+        self.assertTrue(len(blackjGames[2].currentPlayers()) == 1)
+        
 
 if __name__ == '__main__':
     unittest.main()
